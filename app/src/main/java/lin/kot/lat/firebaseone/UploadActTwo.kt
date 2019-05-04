a966623030acb000b2dbf67243f2f2731ab4df2c
Fajar Jr <fajarjunyor13@gmail.com>
2019-05-04 13:03:56 +0700

    storage  ok

--- app/src/main/java/lin/kot/lat/firebaseone/UploadActTwo.kt
+++ app/src/main/java/lin/kot/lat/firebaseone/UploadActTwo.kt
@@ -12,37 +12,55 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.upload_photo.*
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.upload_photo.image
+import kotlinx.android.synthetic.main.upload_photo2.*
 
-class UploadAct : AppCompatActivity() {
+class UploadActTwo : AppCompatActivity() {
 
+
+    lateinit var btnChoose : Button
+    lateinit var btnUpload : Button
+    lateinit var imgView : ImageView
     val PERMISSION_REQUEST_CODE = 1001
-    val PICK_IMAGE_REQUEST = 900
+    val PICK_IMAGE_REQUEST = 71
+    var value = 0.0
     lateinit var filePath : Uri
+    lateinit var storage : FirebaseStorage
+    lateinit var storageReference: StorageReference
 
     @TargetApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.upload_photo)
-        image.setOnClickListener {
+        setContentView(R.layout.upload_photo2)
+        btnChoose = findViewById(R.id.choose)
+        btnUpload = findViewById(R.id.upload)
+        imgView = findViewById(R.id.image)
+
+            btnChoose.setOnClickListener {
             when{
                 (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) -> {
-            if (ContextCompat.checkSelfPermission(this@UploadAct, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED){
-                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
-            }else{
-                chooseFile()
+                    if (ContextCompat.checkSelfPermission(this@UploadActTwo, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+                        requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+                    }else{
+                        chooseImage()
+                    }
                 }
+                else -> chooseImage()
             }
-                else -> chooseFile()
-            }
+        }
+        btnUpload.setOnClickListener {
+            uploadFile()
         }
     }
 
-    private fun chooseFile(){
+    private fun chooseImage(){
         val intent = Intent().apply {
             type = "image/*"
             action = Intent.ACTION_GET_CONTENT
@@ -55,9 +73,9 @@ class UploadAct : AppCompatActivity() {
         when (requestCode){
             PERMISSION_REQUEST_CODE -> {
                 if (grantResults.isEmpty()|| grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    Toast.makeText(this@UploadAct, "Oops! Permission Denied!!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@UploadActTwo, "Oops! Permission Denied!!", Toast.LENGTH_SHORT).show()
                 else
-                    chooseFile()
+                    chooseImage()
             }
         }
     }
@@ -70,7 +88,8 @@ class UploadAct : AppCompatActivity() {
         when (requestCode){
             PICK_IMAGE_REQUEST -> {
                 filePath = data!!.getData()
-                uploadFile()
+                //uploadFile()
+                Toast.makeText(this@UploadActTwo, "File "+filePath+" selected", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -85,23 +104,23 @@ class UploadAct : AppCompatActivity() {
         val data = FirebaseStorage.getInstance()
         var value = 0.0
         var storage = data.getReference()
-            storage.child("mypic.jpg")
+        storage.child("mypic.jpg")
             .putFile(filePath)
             .addOnProgressListener {
-                taskSnapshot ->
+                    taskSnapshot ->
                 value = (100.0 * taskSnapshot.bytesTransferred)/ taskSnapshot
                     .totalByteCount
                 Log.v("value", "value=="+value)
                 progress.setMessage("Upload.." + value.toInt() + "%")
             }
             .addOnSuccessListener {
-                taskSnapshot ->
+                    taskSnapshot ->
                 progress.dismiss()
                 val uri = taskSnapshot.storage.downloadUrl
                 Log.v("Download File", "File.." +uri)
-                Glide.with(this@UploadAct).load(uri).into(image)
+                Glide.with(this@UploadActTwo).load(uri).into(image)
             }.addOnFailureListener {
-                exception -> exception
+                    exception -> exception
                 .printStackTrace()
             }
     }
